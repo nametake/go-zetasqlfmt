42f108ea349b2d9c2715f28a3862742fb887641b
nametake <nametake.kyarabuki@gmail.com>
2024-01-07 08:50:46 +0900

    Parsed packages fset

--- zetasqlfmt.go
+++ zetasqlfmt.go
@@ -3,9 +3,6 @@ package zetasqlfmt
 import (
 	"fmt"
 	"go/ast"
-	"go/parser"
-	"go/token"
-	"go/types"
 	"os"
 	"path/filepath"
 
@@ -38,17 +35,17 @@ type FormatResult struct {
 }
 
 func Format(path string) (*FormatResult, error) {
-	source, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file %s: %v", path, err)
-	}
-
-	fset := token.NewFileSet()
-
-	f, err := parser.ParseFile(fset, path, source, parser.ParseComments)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse file %s: %v", path, err)
-	}
+	// source, err := os.ReadFile(path)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to read file %s: %v", path, err)
+	// }
+	//
+	// fset := token.NewFileSet()
+	//
+	// f, err := parser.ParseFile(fset, path, source, parser.ParseComments)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to parse file %s: %v", path, err)
+	// }
 
 	cfg := &packages.Config{
 		Mode: packages.LoadAllSyntax,
@@ -66,82 +63,57 @@ func Format(path string) (*FormatResult, error) {
 	}
 
 	pkg := pkgs[0]
-	fmt.Println("-------------- Package -------------- ")
 	fmt.Println(pkg.Name)
-	fmt.Println(f.Name.Name)
-	objs := make([]*types.Struct, 0)
-
-	// fmt.Printf("%+v\n", pkg.TypesInfo.Instances)
-	// fmt.Printf("%+v\n", pkg.TypesInfo.Selections)
-	// fmt.Printf("%+v\n", pkg.TypesInfo.Scopes)
-	// fmt.Printf("%+v\n", pkg.TypesInfo.Defs)
-	fmt.Printf("%+v\n", pkg.TypesInfo.Uses)
-	// fmt.Printf("%+v\n", pkg.TypesInfo.Implicits)
-
-	fmt.Println("-------------- Types -------------- ")
-	fmt.Printf("%v\n", pkg.TypesInfo)
-	fmt.Printf("%+v\n", pkg.TypesInfo)
-	fmt.Printf("%#v\n", pkg.TypesInfo)
-
-	for _, typ := range pkg.TypesInfo.Types {
-		fmt.Println("-------------- Type -------------- ")
-		fmt.Println(typ)
-		fmt.Println(typ.Type.Underlying())
-
-		// if s, ok := typ.Type.Underlying().(*types.TypeAndValue); ok {
-		// 	fmt.Println("-------------- Struct -------------- ")
-		// }
-	}
-
-	for _, selection := range pkg.TypesInfo.Selections {
-		fmt.Println("-------------- Selection -------------- ")
-		fmt.Println(selection)
-	}
-
-	for ident, object := range pkg.TypesInfo.Uses {
-		if object.Name() == "SQL" {
-			fmt.Println("-------------- SQL -------------- ")
-			fmt.Println(ident, object)
-			fmt.Println(object.Type().String())
-			fmt.Println(object.Pkg())
-			fmt.Println(object.Parent())
-			fmt.Println(object.Id())
-			fmt.Println(object.Id())
+	// fmt.Printf("%+v\n\n", pkg.TypesInfo.Uses)
+	// fmt.Printf("%+v\n\n", pkg.TypesInfo.Types)
+	// fmt.Printf("%+v\n\n", pkg.TypesInfo.Defs)
+	// fmt.Printf("%+v\n\n", pkg.TypesInfo.Scopes)
+	fmt.Printf("%+v\n\n", pkg.Syntax)
+	for i, f := range pkg.TypesInfo.Uses {
+		fmt.Println("------")
+		if f.Type().String() == "cloud.google.com/go/spanner.Statement" {
+			fmt.Println("@@@@@@@")
+			fmt.Println(i)
 		}
-		if object.Type().String() == "cloud.google.com/go/spanner.Statement" {
-			ast.Inspect(ident, func(n ast.Node) bool {
-				fmt.Println("-------------- Node -------------- ")
-				fmt.Println(n)
-				return true
-			})
-			if s, ok := object.Type().Underlying().(*types.Struct); ok {
-				fmt.Println("Ident:", ident.Obj)
-				objs = append(objs, s)
+	}
+	for _, f := range pkg.Syntax {
+		ast.Inspect(f, func(n ast.Node) bool {
+			// fmt.Printf("%+v\n", n)
+			switch n := n.(type) {
+			case *ast.CompositeLit:
+				fmt.Println("AAAAAAAAAAAAAAAAAAAAAA")
+				sel, ok := n.Type.(*ast.SelectorExpr)
+				if !ok {
+					return true
+				}
+				fmt.Println(pkg.TypesInfo.Uses[sel.Sel])
+				x, ok := sel.X.(*ast.Ident)
+				if !ok {
+					return true
+				}
+				fmt.Println(pkg.TypesInfo.Uses[x])
+				fmt.Println("BBBBBBBBBBBBBBBBBbb")
+
+			case *ast.Ident:
+				//	 fmt.Println("------")
+				use, ok := pkg.TypesInfo.Uses[n]
+				if !ok {
+					return true
+				}
+				if use.Type().String() == "cloud.google.com/go/spanner.Statement" {
+					fmt.Println("@@@@@@@")
+					fmt.Println(n.Name)
+					fmt.Println("1111111111", use)
+				}
+				// fmt.Println(n.Name)
+				// fmt.Println("3333333333", pkg.TypesInfo.Uses[n])
+				// case ast.Expr:
+				//	 fmt.Println("------")
+				//	 fmt.Println(pkg.TypesInfo.Types[n])
 			}
-		}
+			return true
+		})
 	}
-
-	fmt.Println("-------------- Structs -------------- ")
-	fmt.Println(objs)
-	for _, obj := range objs {
-		sql := obj.Field(0)
-		fmt.Println(sql.Type().Underlying())
-	}
-
-	// conf := types.Config{Importer: importer.Default()}
-
-	// info := &types.Info{
-	// 	Uses: make(map[*ast.Ident]types.Object),
-	// }
-
-	// o := f.Scope.Lookup("Statement")
-	// fmt.Println(o)
-	// pkg, err := conf.Check(path, fset, []*ast.File{f}, info)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to type check file %s: %v", path, err)
-	// }
-	//
-	// fmt.Println(pkg)
 
 	return nil, nil
 }
